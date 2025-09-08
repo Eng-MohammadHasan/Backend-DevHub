1c04b549456fe6959fcdd1dc6134ce10e53061db
Mohammad_Hasan <mohammad.hasan.prof@gmail.com>
2025-09-08 07:09:18 +0300

    Create OutputAndVariables.cs

---
+++ OutputAndVariables.cs
@@ -0,0 +1,17 @@
+// Beep sound
+Console.Beep();
+
+//-----------------------------------------------------------------
+// Variables
+//-----------------------------------------------------------------
+int x;              // declaration
+x = 123;            // initialization
+
+short age = 20;     // declaration + initialization
+char symbol = '@';
+string name = "Test";
+string userName = symbol + name;
+
+Console.WriteLine("Your username is : " + userName);
+Console.WriteLine(x);
+Console.WriteLine("My age = " + age);
